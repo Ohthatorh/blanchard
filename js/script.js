a0d7d4b4c5eb6efc6f5aa8c353258627356947a9
ohthatorh <ohthatorh>
2021-01-24 16:44:40 +0300

    upd 24.01

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ $(document).ready(function() {
         }
     });
 
-    $('.header__bot-item-button').click(function(){
+    $('.header__bot-item-button').click(function() {
         $(this).next('.header__bot-item-list').fadeToggle();
     });
 
-    $(document).click(function(e) {
-        if (!$(e.target).closest('.header__bot-item-button').length) {
+    $(document).mouseup(function(e) {
+        if (!$(e.target).closest('.header__bot-item-list').length) {
             $('.header__bot-item-list').fadeOut();
         }
     });
